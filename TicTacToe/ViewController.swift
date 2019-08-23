8bbda7316296dbc6ed73c63da627a3abb0dc6960
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2019-08-23 19:00:14 +0900

    Prepared advance information

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //player1: 1, player2: 2, other: 0.
+    var activePlayer = 1
+    //field information
+    var fieldState = [0,0,0,0,0,0,0,0,0]
+    //winning pattern
+    var winPattern = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    var image = UIImage(named: "Circle.png");
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
