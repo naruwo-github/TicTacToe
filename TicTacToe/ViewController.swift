f085c768acc2a4bacfeafe2aa824468859a3b94e
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2019-08-24 00:06:14 +0900

    Tag number changed

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -18,11 +18,13 @@ class ViewController: UIViewController {
     var fieldState = [0,0,0,0,0,0,0,0,0]
     //winning pattern
     var winPattern = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-    var image = UIImage(named: "Circle.png");
+    var image = UIImage(named: "Circle");
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //print(image!)
     }
     
     //reset button
@@ -33,13 +35,53 @@ class ViewController: UIViewController {
         
         playing = true
         
-        var button: UIButton
-        for i in 0..<9 {
-            button = view.viewWithTag(i) as! UIButton
-            button.setImage(nil, for: .normal)
+        for i in 1..<10 {
+            if let button = self.view.viewWithTag(i) as? UIButton
+            {
+                print(i)
+                button.setBackgroundImage(nil, for: .normal)
+            }
+            //button = view.viewWithTag(i) as! UIButton
+            //button.setBackgroundImage(nil, for: .normal)
         }
         
     }
-
+    
+    @IBAction func cellButtonTapped(_ sender: UIButton) {
+        if fieldState[sender.tag-1] == 0 && playing {
+            fieldState[sender.tag-1] = activePlayer
+            
+            if activePlayer == 1 {
+                image = UIImage(named: "Circle")
+                activePlayer = 2
+            }else if activePlayer == 2 {
+                image = UIImage(named: "Cross")
+                activePlayer = 1
+            }
+            
+            sender.setBackgroundImage(image, for: .normal)
+            print(sender.tag)
+        }
+        
+        for pattern in winPattern {
+            if fieldState[pattern[0]] != 0 && fieldState[pattern[0]] == fieldState[pattern[1]] && fieldState[pattern[0]] == fieldState[pattern[2]]{
+                print("fin")
+                
+                if fieldState[pattern[0]] == 1{
+                    //winLabel.alpha = 1
+                    //winLabel.text = "Twitter won!!"
+                    print("Player1 won!!")
+                    
+                } else if fieldState[pattern[0]] == 2{
+                    //winLabel.alpha = 1
+                    //winLabel.text = "LINE won!!"
+                    print("Player2 won!!")
+                    
+                }
+                playing = false
+            }
+            
+        }
+    }
 }
 
